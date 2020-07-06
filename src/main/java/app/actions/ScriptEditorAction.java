6b5916f950b9c9c3eb2057261c3b849015bbfa02
maxtemplaar@gmail.com <maxtemplaar@gmail.com>
2020-07-06 23:53:18 +0300

    Project classes and resources

---
+++ src/main/java/app/actions/ScriptEditorAction.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.actions;
+
+import java.awt.PopupMenu;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ *
+ * @author MaksK
+ */
+public class ScriptEditorAction extends AbstractAction {
+
+    public ScriptEditorAction() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
